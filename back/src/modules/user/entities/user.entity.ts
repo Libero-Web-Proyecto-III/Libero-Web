c2388fde650193d138b4bfa442a90d4f35518202
DANI <dannyandrestecnologia@outlook.com>
2026-07-28 17:47:37 -0500

    fix: Importaciones de relaciones con EVENT ENTITY y PUBLICATION ENTITY

--- back/src/modules/user/entities/user.entity.ts
+++ back/src/modules/user/entities/user.entity.ts
@@ -1,7 +1,10 @@
 import { ApiHideProperty } from "@nestjs/swagger";
+import { EventEntity } from "src/modules/event/entities/event.entity";
+import { PublicationEntity } from "src/modules/publication/entities/publication.entity";
 import { RolEntity } from "src/modules/rol/entities/rol.entity";
 import { TagEntity } from "src/modules/tag/entities/tag.entity";
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, Generated, JoinColumn, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
 
 @Entity('user')
 export class UserEntity {
@@ -54,7 +57,7 @@ export class UserEntity {
     events: EventEntity[];
 
     @ApiHideProperty()
-    @OneToMany( () => PublicationEntity, (publication), publication.organizer)
+    @OneToMany( () => PublicationEntity, (publication) => publication.author)
     publications: PublicationEntity[];
 
 }

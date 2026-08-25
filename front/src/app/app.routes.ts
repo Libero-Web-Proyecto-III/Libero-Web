69428dae329bd1be3c09fb93b73d726a1e70f394
yuta578 <guevarajhoan47@gmail.com>
2026-08-24 21:42:40 -0500

    feat(front): agregar modulo visual de eventos con tarjetas de ancho completo y sistema de notificaciones

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./events/events.component').then(m => m.EventsComponent)
+  },
+  {
+    path: 'events',
+    loadComponent: () => import('./events/events.component').then(m => m.EventsComponent)
+  },
+  {
+    path: 'eventos',
+    loadComponent: () => import('./events/events.component').then(m => m.EventsComponent)
+  }
+];
